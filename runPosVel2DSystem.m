c7f0946657ec710927c8f887c212bace1f72d712
Patrick Spieler <stapelzeiger@gmail.com>
2015-09-24 23:39:39 +0200

    refactor script

--- runPosVel2DSystem.m
+++ runPosVel2DSystem.m
@@ -21,11 +21,15 @@ System.setVel(omega_0);
 System.setInertia(sys_inertia);
 
 % KF Parameters
+state_names = ['Position'; 'Velocity'];
+
 F = [1, dt
 	 0, 1];
 B = [0;
      0];
 H = eye(2);
+
+Q = diag([0.001, 0.001]);
 
 % Initialisation of KF
 x_0 = [0;
@@ -38,28 +42,28 @@ KF.reset(x_0, P_0)
 time = [t_0];
 x_pred = [x_0]; % KF predictions
 x_est = [x_0]; % KF corrected estimate
-x = [theta_0;
-	 omega_0]; % Exact state of system
+x_exact = [theta_0;
+	       omega_0]; % Exact state of system
 
 P_pred = zeros(2,2,num_steps+1);
-P_est = zeros(2,2,num_steps+1);
+P_estim = zeros(2,2,num_steps+1);
 P_pred(:,:,1) = P_0;
-P_est(:,:,1) = P_0;
+P_estim(:,:,1) = P_0;
 
 % Simulate rotating system and run KF
 for i = 1:num_steps
 	time = [time, i * dt];
 	System.update(0, dt);
 
-	x = [x, System.measurePosVel([0; 0])];
+	x_exact = [x_exact, System.measurePosVel([0; 0])];
 
-	KF.predict(0, zeros(2));
+	KF.predict(0, Q);
 	x_pred = [x_pred, KF.x];
 	P_pred(:,:,i+1) = KF.P;
 
 	KF.measure(System.measurePosVel(noiseStdDev), diag(noiseStdDev.^2));
 	x_est = [x_est, KF.x];
-	P_est(:,:,i+1) = KF.P;
+	P_estim(:,:,i+1) = KF.P;
 end
 
-plot_KF_results(time, x, x_est, P_est, ['Position'; 'Velocity'])
+plot_KF_results(time, x_exact, x_est, P_estim, state_names)
