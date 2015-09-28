3144dd9d87a9a810890a421059c9482e370cdd43
Patrick Spieler <stapelzeiger@gmail.com>
2015-09-28 17:49:55 +0200

    cleanup

--- quatmult_test.m
+++ quatmult_test.m
@@ -1,7 +1,9 @@
 classdef quatmult_test < matlab.unittest.TestCase
     methods (Test)
         function tests(testCase)
-            testCase.verifyEqual(quatmult([1; 2; 3; 4], [2.1; 4.5; 3.2; 1.3]), [-21.7; -0.2; 24.9; 2.6], 'AbsTol', 0.0000001)
+            testCase.verifyEqual(quatmult([1; 2; 3; 4], [2.1; 4.5; 3.2; 1.3]), ...
+                                 [-21.7; -0.2; 24.9; 2.6], ...
+                                 'AbsTol', 0.0000001)
         end
     end
 end
