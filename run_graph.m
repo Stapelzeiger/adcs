019ed156ce71c6c527b4e1108db158612dcb8648
Patrick Spieler <stapelzeiger@gmail.com>
2016-01-23 00:20:42 +0100

    random initial rate for run_graph

--- run_graph.m
+++ run_graph.m
@@ -2,10 +2,8 @@
 delta_t = 0.1; % [s]
 simulation_duration = 120; % [s]
 
-% inertia = [1, 1.1, 1.2];
-inerita_kg_mm = [1.92e3, 2.46e3, 2.34e3]; % kg*mm^2, approx. values form swisscube (Phase D, Status of the ADCS of the SwissCube FM before launch, Hervé Péter-Contesse)
-inerita = inerita_kg_mm / 1e6; % kg*m^2
-initial_rate = [0.01; 0.3; 0];
+inertia_kg_mm = [1.92e3, 2.46e3, 2.34e3]; % kg*mm^2, approx. values form swisscube (Phase D, Status of the ADCS of the SwissCube FM before launch, Hervé Péter-Contesse)
+inertia = inertia_kg_mm / 1e6; % kg*m^2
 measurement_noise = 4/180*pi; % standard deviation of noise on vector components
 perturbation_torque = 9.1e-8; % [Nm], perturbation torque (from Phase 0 CubETH ADCS, Camille Pirat)
 perturbation_torque_noise = perturbation_torque / sqrt(1); % [Nm/sqrt(Hz)] assuming a change every second for white noise density
@@ -24,6 +22,8 @@ att_errors = zeros(nb_runs, simulation_duration/delta_t);
 rate_errors = zeros(nb_runs, simulation_duration/delta_t);
 
 for run_i = 1:nb_runs
+    initial_rate = randn(3,1) * 20/180*pi;
+
     sim = Simulation3DBody(filter_model, ...
                            delta_t, ...
                            inertia, ...
@@ -74,3 +74,10 @@ std_rate_errors = std(rate_errors(:, converged_time/delta_t:end)')
 mean(std_rate_errors)
 max_rate_errors = max(rate_errors(:, converged_time/delta_t:end)')
 max(max_rate_errors)
+
+mean_att_errors = mean(att_errors(:, converged_time/delta_t:end)')
+mean(mean_att_errors)
+std_att_errors = std(att_errors(:, converged_time/delta_t:end)')
+mean(std_att_errors)
+max_att_errors = max(att_errors(:, converged_time/delta_t:end)')
+max(max_att_errors)
